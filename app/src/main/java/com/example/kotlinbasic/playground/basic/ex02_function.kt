cb5a46e041715a3e641972d94100d9b42932a03e
Mew1859 <68700706+Mew1859@users.noreply.github.com>
2020-07-31 12:04:43 +0700

    31/07/63
    part1

---
+++ app/src/main/java/com/example/kotlinbasic/playground/basic/ex02_function.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasic.playground.basic
+
+fun main(){
+    println("hello")
+    //mutable (Implicit) เปลี่ยนค่าได้
+    var var1 ="iBlurBlur"//command + d = copy line
+    var var2 =1234
+    var var3 =true
+    var var4 =12.55f //f or F
+    var var5 =12.55
+    var1="cat"
+}
