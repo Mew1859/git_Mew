2ae35f7ee9c7772d4f32c26c38433effbe7cb5ec
Mew1859 <68700706+Mew1859@users.noreply.github.com>
2020-08-07 11:52:34 +0700

    8/7/63
    8/7/63

--- app/src/main/java/com/example/kotlinbasic/playground/basic/ex02_function.kt
+++ app/src/main/java/com/example/kotlinbasic/playground/basic/ex02_function.kt
@@ -1,12 +1,38 @@
 package com.example.kotlinbasic.playground.basic
 
 fun main(){
-    println("hello")
-    //mutable (Implicit) เปลี่ยนค่าได้
-    var var1 ="iBlurBlur"//command + d = copy line
-    var var2 =1234
-    var var3 =true
-    var var4 =12.55f //f or F
-    var var5 =12.55
-    var1="cat"
+    normalFuntion()
+    argumentFunction(1150,112)
+    println(returnFunction1())
+    println(returnFunction2())
+    println(returnFuction3(112,10))
+    optionalFuction(112)
+    optionalFuction(x2 = 112,x1 = 500)
+
+    //Lambda Expression
+    val  lambdal1 : (Int, Int)->Int={x1, x2 ->x1+x2}//-> =return
+    println(lambdal1(1150,112))
+
+    val  lambdal2  ={x1:Int,x2:Int->x1+x2}
+    println(lambdal2(1150,112))
+
+    //with out lambda
+    fun xxx(x1: Int,x2: Int):Int{
+        return x1+x2
+    }
+
+}fun normalFuntion(){
+     println("normalFunction")
+}
+fun  argumentFunction(x1 :Int,x2 :Int){
+    println("sum: $x1 +$x2")
+    println("sum: ${x1+ x2}")
+}
+fun returnFunction1():String{
+    return "iBlurBlur"
+}
+fun returnFunction2()="iBlurBlur"
+fun returnFuction3(x1: Int,x2: Int)=x1+x2
+fun optionalFuction(x1: Int,x2: Int=0){
+    println("sum: ${x1+x2}")
 }
